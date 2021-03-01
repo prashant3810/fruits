6d8d0169b6594d145a6fe50be3ad79f4a369400c
hari <hari@gmail.com>
2021-03-01 21:52:35 +0530

    added juices_types

--- juices/urls.py
+++ juices/urls.py
@@ -3,5 +3,6 @@ from juices import views
 from django.urls import path
 urlpatterns= [
     path('varaities/', views.varaities, name='varaities'),
-    path('juices_types/',views.varaities_types,name='juices_types')
+    path('juices_colour/',views.varaities_colour,name='juices_colour'),
+    path('juices_types/',views.varaities_types,name='juices_types'),
 ]
